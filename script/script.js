e94b8d004583220b8d125ecf67c4e788831842bf
Jakub <jakub.jadczyk@wp.pl>
2020-09-17 18:58:24 +0200

    navbar change web position + new scrollbar

--- script/script.js
+++ script/script.js
@@ -29,10 +29,70 @@ function scrollFunction() {
         document.querySelector('nav').style.height = '80px';
         document.querySelector('.logo').style.lineHeight = '80px';
         document.querySelector('nav ul').style.lineHeight = '80px';
+        document.querySelector('nav label i').style.lineHeight = '80px';
+        document.querySelector('nav ul').style.top = '80px';
     }
     else{
         document.querySelector('nav').style.height = '120px';
         document.querySelector('.logo').style.lineHeight = '120px';
         document.querySelector('nav ul').style.lineHeight = '120px';
+        document.querySelector('nav label i').style.lineHeight = '120px';
+        document.querySelector('nav ul').style.top = '120px';
+        
         }
 }
+
+
+const navLi = document.querySelectorAll('nav li');
+const navLiList = [...navLi];
+
+
+
+// setInterval(() => {console.log(document.body.scrollTop)}, 1000);
+
+
+// smooth scroll navbar
+
+
+const navbarMenu = document.querySelector('nav ul');
+
+const navbarLinks = document.querySelectorAll('nav ul a');
+
+const burgerUl = document.querySelector('nav i');
+
+
+const followNav = ()=> {
+    console.log(document.querySelector('.cennik').scrollTop);
+};
+
+followNav();
+
+
+const clickBurger = ()=> {
+    burgerUl.click();
+};
+
+
+
+
+const smoothScrool = (e) =>{
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute('href');
+    window.scrollTo({
+        top: document.querySelector(`#${targetId}`).offsetTop,
+        behavior: 'smooth'
+    })
+}
+
+
+
+
+const navbarLinkClick = (e)=>{
+    smoothScrool(e);
+    clickBurger();
+}
+
+
+
+
+navbarLinks.forEach(elem => elem.addEventListener('click', navbarLinkClick));
