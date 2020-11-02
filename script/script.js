b078f85beb9ad7d56b1496c546e5522dca0da962
Jakub <jakub.jadczyk@wp.pl>
2020-11-02 12:27:17 +0100

    update css style

--- script/script.js
+++ script/script.js
@@ -1,6 +1,4 @@
 
-
-
 // shrink navbar
 
 window.onscroll = function() {scrollFunction()};
@@ -26,7 +24,7 @@ function scrollFunction() {
         document.querySelector('nav img').style.height= '100px';
         
         }
-        //dissapear arrow bottom
+        //disappear arrow bottom
         document.documentElement.scrollTop > 20 ? arrowDown.style.display = "none" : arrowDown.style.display = "block"
 }
 
@@ -34,24 +32,16 @@ function scrollFunction() {
 const navLi = document.querySelectorAll('nav li');
 const navLiList = [...navLi];
 
-
-
 // smooth scroll navbar
 
-
 const navbarMenu = document.querySelector('nav ul');
-
 const navbarLinks = document.querySelectorAll('nav ul a');
 const navbarLinksArr = [...navbarLinks];
-
 const burgerUl = document.querySelector('nav i');
-
 const oferta = document.querySelector('.oferta');
 const kadra = document.querySelector('.kadra');
 const cennik = document.querySelector('.cennik');
 const kontakt = document.querySelector('.kontakt');
-
-
 const container = document.querySelector('.container');
 
 
@@ -83,17 +73,10 @@ const followNav = ()=> {
 window.addEventListener('scroll', followNav)
 
 
-
-
-
-
-
 // close hidden menu when click
 const clickBurger = ()=> {
     burgerUl.click();
 };
-
-
 
 // change position using click
 const smoothScrool = (e) =>{
@@ -105,49 +88,12 @@ const smoothScrool = (e) =>{
     })
 }
 
-
 const navbarLinkClick = (e)=>{
     smoothScrool(e);
     clickBurger();
 }
 
 navbarLinks.forEach(elem => elem.addEventListener('click', navbarLinkClick));
-
-
-
-//animated text
-
-// const topText = document.querySelector('.napis');
-// topText.style.opacity = '0.2';
-// let opacity = 0;
-// let flag = true;
-
-
-// const opacityText = ()=>{
-    
-//     if(flag){
-//         console.log(Math.round(opacity*100)/100)
-//         topText.style.opacity = opacity + 0.1;
-//         opacity += 0.1;
-//         if((Math.round(opacity*100)/100) === 1){
-//             flag = !flag;
-//         }
-
-//     }else if(!flag){
-//         console.log(Math.round(opacity*100)/100)
-//         topText.style.opacity = opacity - 0.1;
-//         opacity -= 0.1;
-//         if((Math.round(opacity*100)/100)===0){
-//             flag = !flag;
-//         }
-
-//     }
-
-// };
-
-
-// setInterval(opacityText, 100)
-
 
 //click start take to bottom
 
